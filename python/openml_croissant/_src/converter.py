c264c22d48f17c3ed65123eb4285992452c490e1
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-11-17 19:48:06 +0100

    Added distributions, recordset, and testcase

--- python/openml_croissant/_src/converter.py
+++ python/openml_croissant/_src/converter.py
@@ -1,11 +1,152 @@
+import re
+
 import mlcroissant as mlc
-import openml
+from openml import OpenMLDataFeature, OpenMLDataset
+
+BOOLEAN_STRING_VALUES = ({"TRUE", "FALSE"}, {"1", "0"})
+DATA_FILE_UID = "data-file"
 
 
-def convert(openml_metadata: openml.datasets.OpenMLDataset) -> mlc.nodes.Metadata:
-    return mlc.nodes.Metadata(
-        name=openml_metadata.name,
-        url=openml_metadata.url,
-        citation=openml_metadata.citation,
-        license=openml_metadata.licence,
+def convert(dataset: OpenMLDataset) -> mlc.Metadata:
+    """
+    Convert an openml dataset into a DCF (Croissant) representation.
+
+    Args:
+        dataset: The OpenML dataset metadata
+
+    Returns
+        a (validated) croissant Metadata object.
+
+    Raises:
+        ValueError: Unknown datatype: [openml_datatype].
+        ValueError: Weird datafile format
+    """
+    if not dataset.data_file or not dataset.data_file.endswith("arff"):
+        msg = "Weird datafile format"
+        raise ValueError(msg)
+
+    distributions = [
+        mlc.FileObject(
+            name=DATA_FILE_UID,
+            description="Data file belonging to the dataset.",
+            content_url=dataset.data_file,
+            encoding_format="text/plain",  # No official arff mimetype exist
+            md5=dataset.md5_checksum,
+        ),
+    ]
+
+    record_sets = [
+        mlc.RecordSet(
+            name="data-file-description",
+            fields=[_field(dataset, feature) for feature in dataset.features.values()],
+        ),
+    ]
+
+    return mlc.Metadata(
+        name=dataset.name,
+        description=dataset.description,
+        url=f"https://www.openml.org/search?type=data&id={dataset.dataset_id}",
+        citation=dataset.citation,
+        license=dataset.licence,
+        distribution=distributions,
+        record_sets=record_sets,
     )
+
+
+def _sanitize_name_string(name: str) -> str:
+    """Replace special characters with underscores, and transform to lower case.
+
+    Args:
+        name: a name of a json-ld object.
+
+    Returns:
+        a sanitized version of the name
+    """
+    return re.sub("[^A-Za-z0-9]", "_", name).lower()
+
+
+def _field(dataset: OpenMLDataset, feature: OpenMLDataFeature) -> mlc.Field:
+    """
+    A croissant field description for this OpenML feature.
+
+
+    Args:
+        dataset: The OpenML dataset metadata
+        feature: The OpenML feature metadata
+
+    Returns:
+        A croissant Field
+
+    Raises:
+        ValueError: Unknown datatype: [openml_datatype].
+    """
+    datatypes = _convert_datatype(feature)
+    is_enumeration = feature.nominal_values and datatypes != mlc.DataType.BOOL
+    description = _field_description(dataset, feature)
+
+    return mlc.Field(
+        name=_sanitize_name_string(feature.name),
+        description=description,
+        data_types=datatypes,
+        is_enumeration=is_enumeration,
+        source=mlc.Source(
+            uid=DATA_FILE_UID,
+            node_type="distribution",
+            extract=mlc.Extract(column=feature.name),
+        ),
+    )
+
+
+def _field_description(dataset: OpenMLDataset, feature: OpenMLDataFeature) -> str:
+    """
+    A field description for a feature.
+
+    Args:
+        dataset: The OpenML dataset metadata
+        feature: The OpenML feature metadata
+
+    Returns:
+        A string that can be used as a description of the feature.
+    """
+    if dataset.default_target_attribute and feature.name in dataset.default_target_attribute.split(
+        ",",
+    ):
+        # TODO: should Field.default_target be part of croissant?
+        field_type = "the default target field"
+    elif dataset.row_id_attribute and feature.name in dataset.row_id_attribute:
+        field_type = "the field that uniquely identifies each record, this field should be ignored"
+    elif dataset.ignore_attribute and feature.name in dataset.ignore_attribute:
+        # TODO: should Field.ignored be part of croissant?
+        field_type = "this field should be ignored"
+    else:
+        field_type = "a field"
+    return f"{feature.name} - {field_type}."
+
+
+def _convert_datatype(feature: OpenMLDataFeature) -> mlc.DataType | list[mlc.DataType]:
+    """
+    Convert the datatype according to OpenML to a croissant datatype.
+
+    Args:
+        feature: The OpenML Feature
+
+    Returns:
+        The croissant datatype, or a list of possible croissant datatypes
+
+    Raises:
+        ValueError: Unknown datatype: [openml_datatype].
+    """
+    if feature.nominal_values and any(
+        set(feature.nominal_values).issubset(booleans) for booleans in BOOLEAN_STRING_VALUES
+    ):
+        return mlc.DataType.BOOL
+
+    d_type = {
+        "numeric": [mlc.DataType.FLOAT, mlc.DataType.INTEGER],
+        "string": mlc.DataType.TEXT,
+        "nominal": mlc.DataType.TEXT,  # TODO: where to add the possible values?
+    }.get(feature.data_type, None)
+    if d_type is None:
+        msg = f"Unknown datatype: {feature.data_type}."
+        raise ValueError(msg)
+    return d_type
--- python/openml_croissant/_src/tests/test_converter.py
+++ python/openml_croissant/_src/tests/test_converter.py
@@ -1,15 +1,75 @@
+import mlcroissant as mlc
 import openml
 
 from openml_croissant._src import converter
+from openml_croissant._src.tests.testutils import paths
 
 
 def test_minimal_conversion():
     metadata_openml = openml.datasets.OpenMLDataset(
+        dataset_id="1",
         name="anneal",
         description="description",
-        url="https://example.com/dataset.pq",
         licence="Public",
         citation="https://archive.ics.uci.edu/ml/citation_policy.html",
+        data_file="https://example.com/dataset.arff",
+        md5_checksum="checksum",
     )
     croissant = converter.convert(metadata_openml)
     assert croissant.name == "anneal"
+    assert croissant.description == "description"
+    assert croissant.url == "https://www.openml.org/search?type=data&id=1"
+    assert croissant.license == "Public"
+    assert croissant.citation == "https://archive.ics.uci.edu/ml/citation_policy.html"
+    (distribution,) = croissant.distribution
+    assert distribution.name == "data-file"
+    assert distribution.description == "Data file belonging to the dataset."
+    assert distribution.content_url == "https://example.com/dataset.arff"
+    assert distribution.encoding_format == "text/plain"
+    assert distribution.md5 == "checksum"
+
+
+def test_constructed():
+    file_path_features = paths.path_test_resources() / "openml" / "features_constructed.xml"
+    metadata_openml = openml.datasets.OpenMLDataset(
+        name="test_name",
+        description="Test Description",
+        data_format="arff",  # UNUSED
+        cache_format="pickle",  # UNUSED
+        dataset_id=42,  # UNUSED
+        version=7,  # TODO NO PLACE ?
+        creator="Test Person",  # TODO NO PLACE ?
+        contributor="Another Test Person",  # TODO NO PLACE?
+        collection_date="2020-01-01T00:01:02",  # TODO NO PLACE? test as 2020 or 28/02/1989
+        upload_date="2021-01-01T00:01:02",  # TODO NO PLACE?
+        language="English",  # TODO NO PLACE?s
+        licence="Public",
+        url="https://example.com/url",  # Unused
+        default_target_attribute="target",
+        row_id_attribute="identifier",
+        ignore_attribute=["identifier", "ignored_column"],
+        version_label="Version label",  # Unused
+        citation="Institute of Ontology",
+        tag=["study_1", "study_41", "study_7", "study_88", "uci"],  # Unused
+        visibility=True,  # Unused
+        original_data_url="https://example.com/original-url",  # Unused
+        paper_url="https://example.com/paper-url",  # Unused
+        update_comment="The dataset was uploaded when...",  # Unused
+        md5_checksum="c45bb74cf7ac53ab2b9e61d105dbd454",
+        data_file="https://example.com/data-file.arff",
+        features_file=str(file_path_features),
+        # minio_url=,
+        # features_file: Optional[str] = None,
+        # qualities_file: Optional[str] = None,
+        # dataset = None,
+        # parquet_file: Optional[str] = None
+    )
+
+    croissant_actual = converter.convert(metadata_openml)
+
+    expected_path = paths.path_test_resources() / "croissant" / "constructed.json"
+    issues = mlc.Issues()
+    croissant_expected = mlc.Metadata.from_file(issues, expected_path)
+
+    # print(json.dumps(croissant_actual.to_json(), indent=4))
+    assert croissant_expected.to_json() == croissant_actual.to_json()
